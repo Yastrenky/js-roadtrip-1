a42a8de242870cea1563e14e7115135fe5fc29e0
Yastrenky <ybramos2014@gmail.com>
2017-05-15 19:42:03 +0100

    first commit

---
+++ 1.5_A_Boolean_Expression.js
@@ -0,0 +1,7 @@
+// Video 1.1 accompanies this exercise
+
+// In the console, write a Boolean expression that tests
+// whether two numbers are equal, and whose answer evaluates to false.
+console.log(2 === 3);
+console.log("2" === 2);
+console.log("2" == 2);
